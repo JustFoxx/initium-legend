ad030637c2445e4b640ad3ce3c9ed0523d78c958
JustFoxxo <73485099+JustFoxx@users.noreply.github.com>
2022-05-29 00:17:46 +0200

    check out

--- kubejs/server_scripts/stabilizer.js
+++ kubejs/server_scripts/stabilizer.js
@@ -3,7 +3,7 @@ let max_timer = 200 //measured in ticks(1 second is 20 ticks)[this var applies t
 //runs every player tick
 onEvent('player.tick', event => {
 	//packs the whole code into a function with 2 variables(dim_name and stab_list)
-	function StabilizerLogic(dim_name, stab_list){
+	function StabilizerLogic(dim_name, stab_list) {
 		//checks if the entity is player and if they are in the correct dimension 
 		if (event.entity.type == "minecraft:player" && dim_name) {
 			//checks if the variable stab_timer is less than 1, if it is, makes it one, if it isn't adds 1 and a random number between 0 and 5
@@ -12,13 +12,12 @@ onEvent('player.tick', event => {
 			} else {
 				event.player.persistentData.stab_timer += 1 + (Math.random() * 5)
 			}
-			if (event.player.persistentData.tp_count < 1) {
+			if (event.player.persistentData.tp_count < 1)
 				event.player.persistentData.tp_count = 1
-			}
 			//checks if stab_timer is greater than max_timer
 			if (event.player.persistentData.stab_timer >= max_timer) {
 				//gives player slowness effect with a random time and creates a variable 'i'
-				event.server.runCommandSilent('effect give '+ event.player.name +' minecraft:slowness '+ Math.round(Math.random() * 20 + 5) +' 2 true')
+				event.server.runCommandSilent('effect give ' + event.player.name + ' minecraft:slowness ' + Math.round(Math.random() * 20 + 5) + ' 2 true')
 				event.player.persistentData.i = 0
 				//loops 16 times, or 'i' is greater than 15. Adds 1 for every step
 				for (; event.player.persistentData.i < 15; event.player.persistentData.i++) {
@@ -46,9 +45,9 @@ onEvent('player.tick', event => {
 					//not an air block
 					if (event.level.getBlock(event.player.persistentData.unstable_x, event.player.persistentData.unstable_y, event.player.persistentData.unstable_z) != "minecraft:air" && event.level.getBlock(event.player.persistentData.unstable_x, (event.player.persistentData.unstable_y + 1), event.player.persistentData.unstable_z) == "minecraft:air" && event.level.getBlock(event.player.persistentData.unstable_x, (event.player.persistentData.unstable_y + 2), event.player.persistentData.unstable_z) == "minecraft:air") {
 						//teleports player to unstable coords with sounds and particles
-						event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run particle minecraft:reverse_portal ~ ~ ~ 1 1 1 0.01 250 normal')
-						event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run tp '+ event.player.name +' '+ event.player.persistentData.unstable_x +' '+ (Math.ceil(event.player.persistentData.unstable_y)) +' '+ event.player.persistentData.unstable_z)
-						event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run playsound minecraft:block.respawn_anchor.deplete player '+ event.player.name +' ~ ~ ~')
+						event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run particle minecraft:reverse_portal ~ ~ ~ 1 1 1 0.01 250 normal')
+						event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run tp ' + event.player.name + ' ' + event.player.persistentData.unstable_x + ' ' + (Math.ceil(event.player.persistentData.unstable_y)) + ' ' + event.player.persistentData.unstable_z)
+						event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run playsound minecraft:block.respawn_anchor.deplete player ' + event.player.name + ' ~ ~ ~')
 						//makes stab_timer to 0, adds 1 to tp_count and makes i 15.
 						event.player.persistentData.stab_timer = 0
 						event.player.persistentData.tp_count += 1
@@ -59,9 +58,9 @@ onEvent('player.tick', event => {
 			//checks if the tp_count is equal or greater than 6
 			if (event.player.persistentData.tp_count >= 6) {
 				//teleports player to the void with sounds and particles
-				event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run particle minecraft:squid_ink ~ ~ ~ 1 1 1 0.01 250 normal')
-				event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run tp '+ event.player.name +' ~ -64 ~')
-				event.server.runCommandSilent('execute as '+ event.player.name +' at '+ event.player.name +' run playsound minecraft:block.end_portal.spawn player '+ event.player.name +' ~ ~ ~')
+				event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run particle minecraft:squid_ink ~ ~ ~ 1 1 1 0.01 250 normal')
+				event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run tp ' + event.player.name + ' ~ -64 ~')
+				event.server.runCommandSilent('execute as ' + event.player.name + ' at ' + event.player.name + ' run playsound minecraft:block.end_portal.spawn player ' + event.player.name + ' ~ ~ ~')
 				//makes stab_timer to 0 and tp_count to 0
 				event.player.persistentData.stab_timer = 0
 				event.player.persistentData.tp_count = 0
@@ -74,20 +73,71 @@ onEvent('player.tick', event => {
 			event.player.persistentData.tp_count = 0
 		}
 	}
+	//checks if items exist in inventory
+	const check = (items) => {
+		let i = 0
+		let correctItems = items.map(item => {
+			if (event.player.inventory.count(item) != 0) return 1
+		})
+		if (correctItems.includes(1)) i = 1
+		return 1
+	}
+	//dimensions
+	const bumblezoneDim = { "the_bumblezone:the_bumblezone": true }
+	const netherDim = { "minecraft:the_nether": true }
+	const dungeonDim = {
+		"dimdoors:dungeon_pockets": true,
+		"dimdoors:public_pockets": true,
+		"dimdoors:personal_pockets": true,
+		"dimdoors:limbo": true,
+	}
+	const aetherDim = { "the_aether:the_aether": true }
+	const endDim = { "minecraft:the_end": true }
+	//items required for dimension
+	const bumblezoneItems = [
+		"kubejs:stabilizer_overworld",
+		"kubejs:stabilizer_bumblezone",
+		"kubejs:stabilizer_nether",
+		"kubejs:stabilizer_doors",
+		"kubejs:stabilizer_aether",
+		"kubejs:stabilizer_end"
+	]
+	const netherItems = [
+		"kubejs:stabilizer_bumblezone",
+		"kubejs:stabilizer_nether",
+		"kubejs:stabilizer_doors",
+		"kubejs:stabilizer_aether",
+		"kubejs:stabilizer_end"
+	]
+	const dungeonItems = [
+		"kubejs:stabilizer_nether",
+		"kubejs:stabilizer_doors",
+		"kubejs:stabilizer_aether",
+		"kubejs:stabilizer_end"
+	]
+	const aetherItems = [
+		"kubejs:stabilizer_doors",
+		"kubejs:stabilizer_aether",
+		"kubejs:stabilizer_end"
+	]
+	const endItems = [
+		"kubejs:stabilizer_aether",
+		"kubejs:stabilizer_end"
+	]
 
 	//bumblezone(function version)
-	StabilizerLogic(event.level.dimension == "the_bumblezone:the_bumblezone", (event.player.inventory.count('kubejs:stabilizer_overworld') != 0 || event.player.inventory.count('kubejs:stabilizer_bumblezone') != 0 || event.player.inventory.count('kubejs:stabilizer_nether') != 0 || event.player.inventory.count('kubejs:stabilizer_doors') != 0 || event.player.inventory.count('kubejs:stabilizer_aether') != 0 || event.player.inventory.count('kubejs:stabilizer_end') != 0))
-	
+	StabilizerLogic(bumblezoneDim[event.level.dimension], check(bumblezoneItems))
+
 	//nether (function version)
-	StabilizerLogic(event.level.dimension == "minecraft:the_nether", (event.player.inventory.count('kubejs:stabilizer_bumblezone') != 0 || event.player.inventory.count('kubejs:stabilizer_nether') != 0 || event.player.inventory.count('kubejs:stabilizer_doors') != 0 || event.player.inventory.count('kubejs:stabilizer_aether') != 0 || event.player.inventory.count('kubejs:stabilizer_end') != 0))
-	
+	StabilizerLogic(netherDim[event.level.dimension], check(netherItems))
+
 	//doors (function version)
-	StabilizerLogic((event.level.dimension == "dimdoors:dungeon_pockets" || event.level.dimension == "dimdoors:public_pockets" || event.level.dimension == "dimdoors:personal_pockets" || event.level.dimension == "dimdoors:limbo"), (event.player.inventory.count('kubejs:stabilizer_nether') != 0 || event.player.inventory.count('kubejs:stabilizer_doors') != 0 || event.player.inventory.count('kubejs:stabilizer_aether') != 0 || event.player.inventory.count('kubejs:stabilizer_end') != 0))
-	
+	StabilizerLogic(dungeonDim[event.level.dimension], check(dungeonItems))
+
 	//aether (function version)
-	StabilizerLogic(event.level.dimension == "the_aether:the_aether", (event.player.inventory.count('kubejs:stabilizer_doors') != 0 || event.player.inventory.count('kubejs:stabilizer_aether') != 0 || event.player.inventory.count('kubejs:stabilizer_end') != 0))
-	
+	StabilizerLogic(aetherDim[event.level.dimension], check(aetherItems))
+
 	//end (function version)
-	StabilizerLogic(event.level.dimension == "minecraft:the_end", (event.player.inventory.count('kubejs:stabilizer_aether') != 0 || event.player.inventory.count('kubejs:stabilizer_end') != 0))
-	
+	StabilizerLogic(endDim[event.level.dimension], check(endItems))
+
 })
